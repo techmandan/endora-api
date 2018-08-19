e49ced1cf64b9857631c8e56231e0ba669d45897
Techmandan <dangamerb@gmail.com>
2018-08-19 16:06:53 +0200

    nové demo

--- examples/index.php
+++ examples/index.php
@@ -1,11 +1,13 @@
+<section>
 <?
-/*
 //////////////////////////
 //zakladni             //
 /////////////////////////
 //vytvoreni a pripojeni 
-echo 'zaklad:';
-include('../endora_api.php');//pripoji class
+?>
+  <b>Ukázky</b>  
+<?
+include('../src/endora_api.php');//pripoji class
 $api = new endora_api('ZVcxeVlXNXliMk5wYm5WOGRXVXVZVzFuYVhOcg==');//pripoji API -> dale jako $api
 echo 'Soubory:'.$api->files();//pocet souboru na mem webu
 echo 'diskspace:'.$api->diskspace();//misto na mem webu
@@ -13,16 +15,14 @@ echo 'traffic:'.$api->traffic();//traffic
 //////////////////////////
 //kompletni             //
 //////////////////////////
-echo 'komplet:';
 echo $api->files('limit');//defaultne je current, tedy to co pouzivate. pridanim 'limit' do zavorky zobrazite limit dane hodnoty(zde pocet souboru)
 echo $api->diskspace('current');//je to same jako kdyz napisete echo $api->diskspace();
 echo $api->traffic().'/'.$api->traffic('limit');//casto pouzivane
 echo $api->plan();//zobrazi aktualni plan. Do zavorky se nic nepise
-*/
 //stare demo
 //nove demo:
 ?>
-<section>
+
   <!--for demo wrap-->
   <h1>Kódy</h1>
   <p>Tlustě jsou defaultní hodnoty</p>
